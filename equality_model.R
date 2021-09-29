107ae210d79e998c1b8ac9ebd31c0d9ecdc930dc
Tullia Padellini <tullia.padellini@gmail.com>
2021-09-29 16:38:08 +0100

    init

---
+++ equality_model.R
@@ -0,0 +1,56 @@
+library(INLA)
+
+fin_data <- readRDS(file="data/toy_data.RDS")
+
+
+
+
+# Main analysis for disaggregated BAME subgroups 
+
+formula_main <- counts ~ 1  + rural_urban + vax_prop_stand +
+  IMD_stand + 
+  bame_black_stand + 
+  bame_southasian_stand + 
+  bame_other_stand + 
+  f(age_class, model='iid', constr=TRUE) +
+  f(date_ID, model = "rw2") +
+  f(LTLA_ID, model = "bym2", graph = "data/W.adj", scale.model = TRUE, constr =
+      TRUE, hyper = list(theta1 = list("PCprior", c(1,   0.01)), theta2 =
+                           list("PCprior", c(0.5, 0.5)))) +
+  f(date_LTLA_ID, model = "iid") + 
+  f(date_month_Black, bame_black_stand, model= "rw1", constr = TRUE) +
+  f(date_month_SA, bame_southasian_stand, model= "rw1", constr = TRUE) +
+  f(date_month_Other, bame_other_stand, model= "rw1", constr = TRUE) +
+  f(date_month_IMD, IMD_stand, model= "rw1", constr = TRUE) 
+
+res_main <- inla(formula_main, data = fin_data, family = "binomial",
+                 Ntrials = tot_pop,
+                 verbose = TRUE, 
+                 num.threads = 2, 
+                 control.fixed=list(prec=1,prec.intercept=1),
+                 control.inla=list(int.strategy="eb", strategy="adaptive"))
+
+summary(res_main)
+
+
+# Main analysis for aggregated BAME 
+
+formula_main2 <- counts ~ 1  + rural_urban + vax_prop_stand + 
+  IMD_stand + BAME_stand + 
+  f(age_class, model='iid', constr=TRUE) +
+  f(date_ID, model = "rw2") +
+  f(LTLA_ID, model = "bym2", graph = "data/W.adj", scale.model = TRUE, constr =
+      TRUE, hyper = list(theta1 = list("PCprior", c(1,   0.01)), theta2 =
+                           list("PCprior", c(0.5, 0.5)))) +
+  f(date_LTLA_ID, model = "iid")  +
+  f(date_month, BAME_stand, model= "rw1", constr = TRUE) +
+  f(date_month2, IMD_stand, model= "rw1", constr = TRUE)
+
+res_main2 <- inla(formula_main, data = data_fin, family = "binomial",
+                 Ntrials = tot_pop,
+                 verbose = TRUE, 
+                 num.threads = 2, 
+                 control.fixed=list(prec=1,prec.intercept=1),
+                 control.inla=list(int.strategy="eb", strategy="adaptive"))
+
+summary(res_main2)
